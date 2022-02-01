c8c89504cc63b151cbe465eb1c06e2a675ef812e
Corentin <shinedownizstr8@gmail.com>
2022-02-01 13:15:26 +0100

    changed background image for header and page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Trackmania } from './Trackmania/Trackmania';
 
+const backgroundImage_url =  process.env.PUBLIC_URL + '/images/background/header_background.png'; 
+const wrapper_bkgnd = process.env.PUBLIC_URL + '/images/background/page_background.png';
+
 function App() {
   return (
     <div className='page'>
-      <div className='header'>
+      <div className="header" style={{ backgroundImage: `url(${backgroundImage_url})`, backgroundPosition:'50%' }}>
         Trackmania Trophies
       </div>
-      <div className='wrapper'>
+      <div className='wrapper' style={{ backgroundImage: `url(${wrapper_bkgnd})`, backgroundPosition:'50%' }}>
         <div className='content'>
           <Trackmania/>
         </div>
